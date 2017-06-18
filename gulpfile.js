67913558ba89a7fc36fdec8aa77e93d7a1bbb370
holzschmiede <holzschmiede@tutanota.com>
2017-06-18 14:02:42 +0200

    fix for ftp upload issue

--- gulpfile.js
+++ gulpfile.js
@@ -5,25 +5,32 @@ const gutil = require('gulp-util');
 //
 // variables
 //
-const conn = ftp.create({
-    host: process.env.FTP_HOST,
-    port: process.env.FTP_PORT,
-    user: process.env.FTP_USER,
-    password: process.env.FTP_PASSWORD,
-    parallel  : 1,
-    log: gutil.log,
-    //debug: console.log.bind(console)
-});
-
 const localBuildDir = './build';
 const buildDirGlob = localBuildDir + '/**/*';
 const remotePath = '/';
 const remoteGlob = remotePath + '**';
 
 //
+// helper
+//
+function getFtpConnection() {
+    return ftp.create({
+        host: process.env.FTP_HOST,
+        port: process.env.FTP_PORT,
+        user: process.env.FTP_USER,
+        password: process.env.FTP_PASSWORD,
+        parallel  : 1,
+        log: gutil.log,
+        //debug: console.log.bind(console)
+    });
+}
+
+//
 // tasks
 //
 gulp.task('deploy', function() {
+    const conn = getFtpConnection();
+
     return gulp.src(buildDirGlob, {base: localBuildDir, buffer: false})
         .pipe(conn.newerOrDifferentSize(remotePath))
         .pipe(conn.dest(remotePath))
