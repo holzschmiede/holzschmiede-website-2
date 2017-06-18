3564c46003a89fdc1cf01e84b4eb5e290b01714e
holzschmiede <holzschmiede@tutanota.com>
2017-06-18 23:47:44 +0200

    hopefully final fix for deployment issues

--- gulpfile.js
+++ gulpfile.js
@@ -8,32 +8,29 @@ const gutil = require('gulp-util');
 const localBuildDir = './build';
 const buildDirGlob = localBuildDir + '/**/*';
 const remotePath = '/holzschmiede/';
-const remoteGlob = remotePath + '**';
+const remoteGlob = remotePath + '/**/*';
 
-//
-// helper
-//
-function getFtpConnection() {
-    return ftp.create({
-        host: process.env.FTP_HOST,
-        port: process.env.FTP_PORT,
-        user: process.env.FTP_USER,
-        password: process.env.FTP_PASSWORD,
-        parallel  : 1,
-        log: gutil.log,
-        //debug: console.log.bind(console)
-    });
-}
+const conn = ftp.create({
+    host: process.env.FTP_HOST,
+    port: process.env.FTP_PORT,
+    user: process.env.FTP_USER,
+    password: process.env.FTP_PASSWORD,
+    parallel  : 1,
+    log: gutil.log,
+    //debug: console.log.bind(console)
+});
 
 //
 // tasks
 //
-gulp.task('deploy', function() {
-    const conn = getFtpConnection();
-
+gulp.task('upload', function() {
     return gulp.src(buildDirGlob, {base: localBuildDir, buffer: false})
         .pipe(conn.newerOrDifferentSize(remotePath))
-        .pipe(conn.dest(remotePath))
-        .pipe(conn.clean(remoteGlob, localBuildDir, {base: remotePath}));
+        .pipe(conn.dest(remotePath));
 });
 
+gulp.task('remove-dir', function (cb) {
+    conn.rmdir(remotePath, cb);
+});
+
+gulp.task('deploy', ['remove-dir', 'upload']);
